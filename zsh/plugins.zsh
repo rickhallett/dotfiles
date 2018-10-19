5c0ade76e4433d6c82f3ce48b9d81a449cd524f8
Matthew Franglen <matthew@franglen.org>
2018-10-19 21:57:50 +0100

    Drop unused plugins

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -6,18 +6,11 @@ antigen_bundles+=(
     Tarrasch/zsh-functional
     bobthecow/git-flow-completion
     https://gitlab.com/matthewfranglen/docker-go
-    https://gitlab.com/matthewfranglen/easy-ln
     https://gitlab.com/matthewfranglen/format-python
     https://gitlab.com/matthewfranglen/git-stashes
     https://gitlab.com/matthewfranglen/git-update
-    https://gitlab.com/matthewfranglen/memcached-cli
     https://gitlab.com/matthewfranglen/random
-    https://gitlab.com/matthewfranglen/show-server-cert
-    https://gitlab.com/matthewfranglen/spark
-    https://gitlab.com/matthewfranglen/watch-me
-    https://gitlab.com/matthewfranglen/window-to-gif
     matthewfranglen/gitflow-avh --branch=master
-    matthewfranglen/speedread
     supercrabtree/k
     zsh-users/zsh-autosuggestions
     zsh-users/zsh-history-substring-search
