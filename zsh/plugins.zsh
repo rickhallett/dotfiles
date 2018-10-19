760b1e49e91bef4a502cecb7946a31fee14690be
Matthew Franglen <matthew@franglen.org>
2018-10-19 22:27:40 +0100

    Add profiling to plugin load

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,7 +1,11 @@
+_DEBUG_PLUGINS=
+
 function install_plugins() {
+    plugin::log "Starting..."
     source ~/.antigen.zsh
 
     [ -e ~/.antigen-plugins.zsh ] && source ~/.antigen-plugins.zsh
+    plugin::log "Registered external plugins"
 
     antigen_bundles+=(
         Tarrasch/zsh-colors
@@ -30,21 +34,43 @@ function install_plugins() {
     do
         antigen-bundle ${=bundle}
     done
+    plugin::log "Registered bundles"
     unset antigen_bundles
 
     antigen-apply
+    plugin::log "Applied plugins"
 
     # Set the autocomplete color for zsh-autocomplete.
     # Has to be done after loading.
     export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=5"
 
     source ~/.fzf.zsh
+    plugin::log "Sourced fzf"
 }
+
+if [ -n "${_DEBUG_PLUGINS}" ]; then
+    PLUGIN_START_TIME="$(date "+%s.%N")"
+    PLUGIN_LAST_TIME="${PLUGIN_START_TIME}"
+
+    function plugin::log {
+        local current_time="$(date "+%s.%N")"
+        debug::log "plugins" "${PLUGIN_START_TIME}" "${PLUGIN_LAST_TIME}" "${current_time}" "${1}"
+        PLUGIN_LAST_TIME="${current_time}"
+    }
+else
+    function plugin::log {
+
+    }
+fi
 
 if ! which antigen >/dev/null; then
     install_plugins
 fi
 
 unfunction install_plugins
+unfunction plugin::log
+unset PLUGIN_START_TIME
+unset PLUGIN_LAST_TIME
+unset _DEBUG_PLUGINS
 
 # vim: set ai et sw=4 syntax=zsh :
