487cb39a01bca89e74dc0832ae3ea3fdc23691f9
Matthew Franglen <matthew@franglen.org>
2019-09-15 15:37:41 +0100

    Add conda config to python config
    At this point the shell launch speed averages around 0.44
    (I'm training bert at the same time on this computer and blaming that
    for the variance):
    
    ➜ for i in $(seq 1 10); do /usr/bin/time --format '%E real\t%U user\t%S
    system' zsh -i -c exit; done
    0:00.46 real    0.39 user       0.11 system
    0:00.56 real    0.41 user       0.08 system
    0:00.67 real    0.35 user       0.11 system
    0:00.76 real    0.37 user       0.09 system
    0:00.44 real    0.37 user       0.10 system
    0:00.42 real    0.34 user       0.13 system
    0:00.43 real    0.36 user       0.10 system
    0:00.61 real    0.36 user       0.13 system
    0:00.42 real    0.37 user       0.10 system
    0:00.44 real    0.36 user       0.11 system

--- python/config.zsh
+++ python/config.zsh
@@ -6,3 +6,32 @@ _pipenv() {
   eval $(env COMMANDLINE="${words[1,$CURRENT]}" _PIPENV_COMPLETE=complete-zsh  pipenv)
 }
 compdef _pipenv pipenv
+
+function python::setup_conda {
+    # from conda init
+    __conda_setup="$("${CONDA_HOME}/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "${CONDA_HOME}/etc/profile.d/conda.sh" ]; then
+            . "${CONDA_HOME}/etc/profile.d/conda.sh"
+        else
+            export PATH="${CONDA_HOME}/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+}
+
+if [[ -z "${CONDA_HOME}" ]]; then
+    if [[ -e "${HOME}/Programming/Python/anaconda3" ]]; then
+        export CONDA_HOME="${HOME}/Programming/Python/anaconda3"
+        export PATH="${CONDA_HOME}/bin:${PATH}"
+        python::setup_conda
+    fi
+# https://unix.stackexchange.com/questions/411304/how-do-i-check-whether-a-zsh-array-contains-a-given-value
+elif [[ ${path[(ie)${CONDA_HOME}/bin]} -gt ${#path} ]]; then
+    export PATH="${CONDA_HOME}/bin:${PATH}"
+    python::setup_conda
+fi
+
+unfunction python::setup_conda
