485372023dd34b1cd40b6d8bf32b26b3637c9b3e
Matthew Franglen <matthew@franglen.org>
2019-09-26 23:31:40 +0100

    Install formatter and linter

--- rust/install.sh
+++ rust/install.sh
@@ -5,7 +5,9 @@ set -eu
 readonly CARGO_FOLDER="${HOME}/.cargo"
 
 install () {
-    install_rust || return ${STATUS_ERROR}
+    install_rust           || return ${STATUS_ERROR}
+    install_rust_formatter || return ${STATUS_ERROR}
+    install_rust_linter    || return ${STATUS_ERROR}
 
     return ${STATUS_OK}
 }
@@ -13,6 +15,18 @@ install () {
 install_rust () {
     if [ ! -e "${CARGO_FOLDER}" ]; then
         curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
+    fi
+}
+
+install_rust_formatter () {
+    if ! "${CARGO_FOLDER}/bin/rustup" component list | grep rustfmt >/dev/null; then
+        "${CARGO_FOLDER}/bin/rustup" component add rustfmt
+    fi
+}
+
+install_rust_linter () {
+    if ! "${CARGO_FOLDER}/bin/rustup" component list | grep clippy >/dev/null; then
+        "${CARGO_FOLDER}/bin/rustup" component add clippy
     fi
 }
 
