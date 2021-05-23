16488a782c34029118aa4252604e5d1048f3b86b
Matthew Franglen <matthew@franglen.org>
2021-05-23 11:06:39 +0100

    Update pyenv appropriately
    This is likely to be folded into oh-my-zsh soon so should be reverted

--- python/config.zsh
+++ python/config.zsh
@@ -7,6 +7,7 @@ if [ -e "${HOME}/.pyenv" ]; then
     export PYENV_ROOT="$HOME/.pyenv"
     export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init --path)"
+    eval "$(pyenv init -)"
 fi
 
 function conda {
