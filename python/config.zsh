a5726fb533b68975ae8e14bc73054b2030ec76e7
Matthew Franglen <matthew@franglen.org>
2021-05-25 08:10:49 +0100

    FUCK OFF PYENV

--- python/config.zsh
+++ python/config.zsh
@@ -6,7 +6,6 @@ export PATH="${PATH}:${HOME}/.cargo/bin"
 if [ -z "${PYENV_ROOT}" ]; then
     export PYENV_ROOT="$HOME/.pyenv"
     export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init --path)"
 fi
 
 eval "$(pyenv init -)"
