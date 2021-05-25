95ff104f2bbdc0d154a3af470fb543fceabf9134
Matthew Franglen <matthew@franglen.org>
2021-05-25 07:46:58 +0100

    Make pyenv happy?

--- python/config.zsh
+++ python/config.zsh
@@ -3,12 +3,7 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 export CONDA_INITIALIZED=
 export PATH="${PATH}:${HOME}/.cargo/bin"
 
-if [ -e "${HOME}/.pyenv" ]; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init --path)"
-    eval "$(pyenv init -)"
-fi
+eval "$(pyenv init -)"
 
 function conda {
     if [[ -z "${CONDA_HOME}" ]]; then
