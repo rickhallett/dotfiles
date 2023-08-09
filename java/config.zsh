5244f5ec0400ec787e0c9f55e7e56f9e82048d8e
Matthew Franglen <matthew@franglen.org>
2023-08-09 11:42:55 +0100

    move sdkman faff into java

--- java/config.zsh
+++ java/config.zsh
@@ -1,1 +1,6 @@
-[[ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"
+if [ -e "${HOME}/.sdkman" ]; then
+    export SDKMAN_DIR="${HOME}/.sdkman"
+    unsetopt nounset
+    [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+    setopt nounset
+fi
