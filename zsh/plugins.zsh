6fcdbd2b33b7492ba457f723b33262cc507d0421
Matthew Franglen <matthew@franglen.org>
2023-08-09 09:28:07 +0100

    Only load sdkman if it is present

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -9,8 +9,10 @@ function install_plugins() {
     # Has to be done after loading.
     export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=5"
 
-    export SDKMAN_DIR="${HOME}/.sdkman"
-    [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+    if [ -e "${HOME}/.sdkman" ]; then
+        export SDKMAN_DIR="${HOME}/.sdkman"
+        [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+    fi
 }
 
 if [ -n "${_DEBUG_PLUGINS}" ]; then
