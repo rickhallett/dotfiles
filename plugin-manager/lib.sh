faaf2a20df42b4c05e900848c5be21593e145d29
Matthew Franglen <matthew@franglen.org>
2023-08-09 10:14:16 +0100

    Use lib.sh for git and github-release

--- plugin-manager/lib.sh
+++ plugin-manager/lib.sh
@@ -1,23 +1,31 @@
 set -euo pipefail
 
 git_repo_folder () {
-    local CACHE_FOLDER="${XDG_CACHE_DIR:-${HOME}/.cache}/dotfiles"
-    local CLONE_CACHE_FOLDER="${CACHE_FOLDER}/repos/clones"
-
     local URL="${1?You must provide the https url to use}"
     local NAME="${URL:gs#/#--#:gs#:##}"
+
+    local CACHE_FOLDER="${XDG_CACHE_DIR:-${HOME}/.cache}/dotfiles"
+    local CLONE_CACHE_FOLDER="${CACHE_FOLDER}/repos/clones"
     local FOLDER_NAME="${CLONE_CACHE_FOLDER}/${NAME}"
+
+    if [ ! -e "${CLONE_CACHE_FOLDER}" ]; then
+        mkdir -p "${CLONE_CACHE_FOLDER}"
+    fi
 
     echo "${FOLDER_NAME}"
 }
 
 github_release_folder () {
-    local CACHE_FOLDER="${XDG_CACHE_DIR:-${HOME}/.cache}/dotfiles"
-    local RELEASE_CACHE_FOLDER="${CACHE_FOLDER}/repos/releases"
-
     local REPO_NAME="${1?You must provide the repo to use}"
     local NAME="${REPO_NAME:gs#/#--#:gs#:##}"
+
+    local CACHE_FOLDER="${XDG_CACHE_DIR:-${HOME}/.cache}/dotfiles"
+    local RELEASE_CACHE_FOLDER="${CACHE_FOLDER}/repos/releases"
     local FOLDER_NAME="${RELEASE_CACHE_FOLDER}/${NAME}"
+
+    if [ ! -e "${RELEASE_CACHE_FOLDER}" ]; then
+        mkdir -p "${RELEASE_CACHE_FOLDER}"
+    fi
 
     echo "${FOLDER_NAME}"
 }
@@ -25,6 +33,8 @@ github_release_folder () {
 log_file () {
     local FOLDER_NAME="${1?You must provide the folder name to use}"
     local NAME="${FOLDER_NAME##*/}"
+
+    local CACHE_FOLDER="${XDG_CACHE_DIR:-${HOME}/.cache}/dotfiles"
     local LOG_FOLDER="${CACHE_FOLDER}/logs"
     if [ ! -e "${LOG_FOLDER}" ]; then
         mkdir "${LOG_FOLDER}"
